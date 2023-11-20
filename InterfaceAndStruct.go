1d4e44029e08b97bef01f8ffe82441f01e3ccde5
Robin Joseph <robinjoseph11998@gmail.com>
2023-11-20 13:02:05 +0530

    commit4

---
+++ InterfaceAndStruct.go
@@ -0,0 +1,32 @@
+package main
+
+// import "fmt"
+
+// type Example interface {
+// 	Adder(a int, b int) int
+// }
+
+// type Fields struct {
+// 	a int
+// 	b int
+// }
+
+// func (f *Fields) Adder(a int, b int) int {
+
+// 	f.a = a
+// 	f.b = b
+
+// 	sum := f.a + f.b
+
+// 	return sum
+// }
+
+// func main() {
+
+// 	obj := Fields{}
+
+// 	sum := obj.Adder(10, 30)
+
+// 	fmt.Println("Result:", sum)
+
+// }
--- main.go
+++ main.go
@@ -5,24 +5,23 @@ import (
 	"time"
 )
 
-func Adder(a int, b int, ch chan<- int) {
-	ch <- a + b
+func Adder(a, b int, ch chan<- int) {
 
+	ch <- a + b
 }
 
-func Divider(ch <-chan int) {
-
+func SquareFinder(ch <-chan int) {
 	sum := <-ch
-	result := sum / 7
-	fmt.Println("Result", result)
+	result := sum * sum
+	fmt.Println("square Is", result)
 }
 
 func main() {
-	fmt.Println("Start")
+	fmt.Println("START")
 	channel := make(chan int)
-	go Adder(20, 100, channel)
-	go Divider(channel)
-	time.Sleep(3 * time.Second)
-	fmt.Println("End")
+	go Adder(3, 2, channel)
+	go SquareFinder(channel)
+	time.Sleep(1 * time.Second)
 
+	fmt.Println("END")
 }
