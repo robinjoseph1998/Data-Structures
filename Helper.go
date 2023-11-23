bb094c40f868a1a9445e4b2d56e82d05f6ee5670
Robin Joseph <robinjoseph11998@gmail.com>
2023-11-24 01:43:05 +0530

    test commit

--- Helper.go
+++ Helper.go
@@ -1,22 +1,38 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"sort"
-// )
-
-// func minimumDifference(nums []int, k int) int {
-// 	if len(nums)-1 == 0 {
-// 		return 0
+// func maximumDifference(nums []int) int {
+// 	fmt.Println("nums", nums)
+// 	size := len(nums) - 1
+// 	min := math.MaxInt64
+// 	max := math.MinInt64
+// 	diff := []int{}
+// 	var i, j int
+// 	for i = 0; i <= size; i++ {
+// 		if nums[i] < min {
+// 			min = nums[i]
+// 			fmt.Println("min", min)
+// 			break
+// 		}
 // 	}
-// 	sort.Ints(nums)
-// 	L := len(nums) - 1
-// 	SL := len(nums) - 2
+// 	for j = size; j >= size; j-- {
+// 		if nums[j] > max {
+// 			max = nums[j]
+// 			fmt.Println("max", max)
+// 			break
+// 		}
 
-// 	Diff := nums[L] - nums[SL]
-// 	return Diff
-// }
-// func main() {
-// 	nums := []int{90}
-// 	fmt.Println("Ans", minimumDifference(nums, 1))
+// 		if i < j {
+// 			diff = append(diff, nums[j]-nums[i])
+// 		} else {
+// 			fmt.Println("diff", diff)
+// 			return -1
+// 		}
+// 	}
+// 	m := math.MinInt64
+// 	for _, val := range diff {
+// 		if val > m {
+// 			m = val
+// 		}
+// 	}
+// 	return m
 // }
--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
-func maximumDifference(nums []int) int {
+import "fmt"
 
+func construct2DArray(original []int, m int, n int) [][]int {
+	var TwoD [][]int
+	var k int
+	p := 1
+	for i := 0; i < m; i++ {
+		var row []int
+		for j := 0 + k; j < n*p; j++ {
+			row = append(row, original[j])
+		}
+		k++
+		p = n
+		TwoD = append(TwoD, row)
+	}
+	return TwoD
 }
-
 func main() {
-	nums := []int{7, 1, 5, 4}
-	maximumDifference(nums)
+	m := 2
+	n := 2
+	orginal := []int{1, 2, 3, 4}
+	fmt.Println(construct2DArray(orginal, m, n))
 
 }
