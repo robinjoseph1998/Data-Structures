db3792d245d69ed90391e9fafb266f1ff7e0a53b
Robin Joseph <robinjoseph11998@gmail.com>
2023-11-25 00:59:59 +0530

    some changes

--- main.go
+++ main.go
@@ -2,25 +2,38 @@ package main
 
 import "fmt"
 
-func construct2DArray(original []int, m int, n int) [][]int {
-	var TwoD [][]int
-	var k int
-	p := 1
-	for i := 0; i < m; i++ {
-		var row []int
-		for j := 0 + k; j < n*p; j++ {
-			row = append(row, original[j])
+func twoOutOfThree(nums1 []int, nums2 []int, nums3 []int) []int {
+	var BigNums [][]int
+	BigNums = append(BigNums, nums1, nums2, nums3)
+	i := 0
+	var res []int
+	var result []int
+	for i <= len(BigNums)-1 {
+		arr := BigNums[i]
+		helpMap := make(map[int]bool)
+		for _, val := range arr {
+			if !helpMap[val] {
+				helpMap[val] = true
+				result = append(result, val)
+			}
 		}
-		k++
-		p = n
-		TwoD = append(TwoD, row)
+		i++
 	}
-	return TwoD
+	ResultMap := make(map[int]int)
+	for _, val := range result {
+		ResultMap[val]++
+	}
+	for key, val := range ResultMap {
+		if val > 1 {
+			res = append(res, key)
+		}
+	}
+	return res
 }
 func main() {
-	m := 2
-	n := 2
-	orginal := []int{1, 2, 3, 4}
-	fmt.Println(construct2DArray(orginal, m, n))
+	nums1 := []int{1, 1, 3, 2}
+	nums2 := []int{2, 3}
+	nums3 := []int{3}
 
+	fmt.Println(twoOutOfThree(nums1, nums2, nums3))
 }
