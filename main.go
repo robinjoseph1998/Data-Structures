f07d293dc17d2a0ee0bd6eaf87fe156a3246c0b4
Robin Joseph <robinjoseph11998@gmail.com>
2023-12-13 01:55:11 +0530

    latest commit

--- main.go
+++ main.go
@@ -1,46 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-	"strconv"
-	"strings"
-)
+import "fmt"
 
-func largestGoodInteger(num string) string {
-	var numInt []int
-	for i := 0; i < len(num); i++ {
-		val, _ := strconv.Atoi(string(num[i]))
-		numInt = append(numInt, val)
-	}
-	var Triples []int
-	for i := 0; i < len(numInt); i++ {
-		EachstrVal := strconv.Itoa(numInt[i])
-		TriplledStr := ""
-		for i := 1; i <= 3; i++ {
-			TriplledStr += EachstrVal
-		}
-		if strings.Contains(num, TriplledStr) {
-			tripleIntegers, _ := strconv.Atoi(TriplledStr)
-			Triples = append(Triples, tripleIntegers)
+func percentageLetter(s string, letter byte) int {
+	var count int
+	for i := 0; i < len(s); i++ {
+		if s[i] == letter {
+			count++
 		}
 	}
-	if len(Triples) == 0 {
-		return ""
+	if count == 0 {
+		return 0
 	}
-	sort.Ints(Triples)
-	val := Triples[len(Triples)-1]
-	if val == 0 {
-		return "000"
-	}
-	str := strconv.Itoa(val)
-	return str
+	return (count * 100) / 6
 }
 
 func main() {
 
-	num := "2300019"
-
-	fmt.Println(largestGoodInteger(num))
+	s := "kue"
+	letter := byte('e')
+	fmt.Println(percentageLetter(s, letter))
 
 }
