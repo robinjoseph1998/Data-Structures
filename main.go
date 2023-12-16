c0d6247e55f741bbdb7b1427d8032b1d417bb671
Robin Joseph <robinjoseph11998@gmail.com>
2023-12-16 14:21:59 +0530

    latest commit

--- main.go
+++ main.go
@@ -1,24 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func percentageLetter(s string, letter byte) int {
+func digitCount(num string) bool {
+	if len(num) == 1 {
+		return false
+	}
+	var intNum []int
+	for _, val := range num {
+		digit := int(val - '0')
+		intNum = append(intNum, digit)
+	}
+	fmt.Println("intNum", intNum)
 	var count int
-	for i := 0; i < len(s); i++ {
-		if s[i] == letter {
-			count++
+	for i := 0; i < len(intNum); i++ {
+		for k := 0; k < len(intNum); k++ {
+			if i == intNum[k] {
+				fmt.Println(i, intNum[k])
+				count++
+			}
+		}
+		fmt.Println("count", count)
+		if count != int(num[i]) {
+			return false
 		}
 	}
-	if count == 0 {
-		return 0
-	}
-	return (count * 100) / 6
+
+	return true
 }
-
 func main() {
-
-	s := "kue"
-	letter := byte('e')
-	fmt.Println(percentageLetter(s, letter))
-
+	num := "1210"
+	fmt.Println(digitCount(num))
 }
