a3e401af7b0489392a47880e55de4c8d90ff0a0d
Robin Joseph <robinjoseph11998@gmail.com>
2023-12-26 02:15:02 +0530

    latest commit

--- main.go
+++ main.go
@@ -2,46 +2,24 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func sortString(s string) string {
-	result := ""
-	var Hlp1 []int
-	var k, Forward, Backward int
-	for {
-		if k == 2 {
-			break
-		}
-		for i := 0; i < len(s); i++ {
-			Hlp1 = append(Hlp1, int(s[i]))
-		}
-		sort.Ints(Hlp1)
-		result += string(s[0])
-		for i := 0; i < len(Hlp1); i++ {
-			if i+1 < len(s) {
-				if s[i] != s[i+1] {
-					result += string(s[i+1])
-					Forward++
-				}
-			}
-			if Forward == 2 {
-				break
-			}
-		}
-		for i := len(result) - 1; i >= 0; i-- {
-			if i >= 0 {
-				result += string(result[i])
-				Backward++
-			}
-			if Backward == 2 {
-				break
-			}
-		}
-		k++
+	var temp []int
+	for i := 0; i < len(s); i++ {
+		temp = append(temp, int(s[i]))
 	}
-	size := len(result)
-	return result[:size/1]
+	for i := 0; i < len(s); i++ {
+		if temp[i] > temp[i+1] {
+
+		}
+
+	}
+	result := ""
+	for i := 0; i < len(s); i++ {
+		result += string(temp[i])
+	}
+	return result
 }
 
 func main() {
