7f370c800ea8421d5579913d18cc320d120d4016
Robin Joseph <robinjoseph11998@gmail.com>
2023-12-26 14:55:05 +0530

    latest commit

--- main.go
+++ main.go
@@ -2,27 +2,37 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"sort"
 )
 
-func sortString(s string) string {
-	var temp []int
-	for i := 0; i < len(s); i++ {
-		temp = append(temp, int(s[i]))
-	}
-	for i := 0; i < len(s); i++ {
-		if temp[i] > temp[i+1] {
-
+func findMaxK(nums []int) int {
+	sort.Ints(nums)
+	fmt.Println("nums", nums)
+	var result int
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] < 0 {
+				pInt := int(math.Abs(float64(nums[i])))
+				if pInt == nums[j] {
+					fmt.Println("pInt", pInt)
+					if result < pInt {
+						result = pInt
+					}
+				} else {
+					continue
+				}
+			}
 		}
-
 	}
-	result := ""
-	for i := 0; i < len(s); i++ {
-		result += string(temp[i])
+	if result != 0 {
+		return result
 	}
-	return result
+	return -1
 }
 
 func main() {
-	s := "rat"
-	fmt.Println(sortString(s))
+
+	nums := []int{-1, 10, 6, 7, -7, 1}
+	fmt.Println(findMaxK(nums))
 }
