7d9ed9ea4c064a13545e09d050ab953c5a96fd4a
Robin Joseph <robinjoseph11998@gmail.com>
2024-01-01 16:15:25 +0530

    latest commit

--- main.go
+++ main.go
@@ -2,44 +2,27 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strconv"
 )
 
-func minimizeSet(divisor1 int, divisor2 int, uniqueCnt1 int, uniqueCnt2 int) int {
-	gcd := func(a, b int) int {
-		for b != 0 {
-			a, b = b, a%b
+func alternateDigitSum(n int) int {
+	s := strconv.Itoa(n)
+	var Total int
+	for index, val := range s {
+		num, _ := strconv.Atoi(string(val))
+		if index%2 == 0 {
+			Total += num
+			fmt.Println("-num", -num)
+		} else {
+			Total += -num
+			fmt.Println("num", num)
 		}
-		return a
 	}
-
-	LCM := int(math.Abs(float64(divisor1*divisor2)) / float64(gcd(divisor1, divisor2)))
-	TotalCount := uniqueCnt1 + uniqueCnt2
-
-	MaxArr1 := uniqueCnt1 + uniqueCnt1/(divisor1-1)
-	if uniqueCnt1%(divisor1-1) == 0 {
-		MaxArr1--
-	}
-	MaxArr2 := uniqueCnt2 + uniqueCnt2/(divisor2-1)
-	if uniqueCnt2%(divisor2-1) == 0 {
-		MaxArr2--
-	}
-
-	MaxBoth := TotalCount + TotalCount/(LCM-1)
-	if TotalCount%(LCM-1) == 0 {
-		MaxBoth--
-	}
-
-	return int(math.Max(float64(MaxArr1), math.Max(float64(MaxArr2), float64(MaxBoth))))
+	return Total
 }
 
 func main() {
-
-	divisor1 := 2
-	divisor2 := 4
-	uniqueCnt1 := 8
-	uniqueCnt2 := 2
-
-	fmt.Println(minimizeSet(divisor1, divisor2, uniqueCnt1, uniqueCnt2))
+	n := 25
+	fmt.Println(alternateDigitSum(n))
 
 }
