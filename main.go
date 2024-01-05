4835b191653f3cab335941fbf0d0dc2de846f2d1
Robin Joseph <robinjoseph11998@gmail.com>
2024-01-05 12:08:30 +0530

    latest commit

--- main.go
+++ main.go
@@ -2,44 +2,29 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"strconv"
 )
 
-func minOperations(nums1 []int, nums2 []int, k int) int64 {
-	if k == 0 {
-		if reflect.DeepEqual(nums1, nums2) {
-			return 0
-		}
-		return -1
-	}
-
-	var positive, negative int
-
-	for i := 0; i < len(nums1); i++ {
-		diff := nums1[i] - nums2[i]
-
-		if diff%k != 0 {
-			return -1
-		}
-
-		if diff > 0 {
-			positive += diff / k
-		} else if diff < 0 {
-			negative -= diff / k
+func separateDigits(nums []int) []int {
+	var result []int
+	for _, val := range nums {
+		if val > 9 {
+			str := strconv.Itoa(val)
+			for _, char := range str {
+				intval := int(char)
+				c, _ := strconv.Atoi(string(intval))
+				result = append(result, c)
+			}
 		} else {
-			continue
+			result = append(result, val)
+			fmt.Println("Trees", result)
 		}
 	}
-	if positive != negative {
-		return -1
-	}
-	return int64(positive)
+	fmt.Println("result", result)
+	return result
 }
 
 func main() {
-	nums1 := []int{4, 3, 1, 4}
-	nums2 := []int{1, 3, 7, 1}
-	k := 1
-	fmt.Println(minOperations(nums1, nums2, k))
-
+	nums := []int{9, 81, 33, 17, 58, 42, 8, 75}
+	fmt.Println(separateDigits(nums))
 }
