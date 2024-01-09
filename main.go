b65bb0918c20b4fd31cd0808397b70d35075522f
Robin Joseph <robinjoseph11998@gmail.com>
2024-01-09 17:05:12 +0530

    latest commit

--- main.go
+++ main.go
@@ -2,40 +2,24 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 )
 
-func findTheArrayConcVal(nums []int) int64 {
-	i := 0
-	var str string
-	var sum int
-	ed := len(nums) - 1
-	for i < len(nums) {
-		st := i
-		if i != ed {
-			str += strconv.Itoa(nums[st])
-			fmt.Println("str", str)
-			str += strconv.Itoa(nums[ed])
-			val, _ := strconv.Atoi(str)
-			fmt.Println("val", val)
-			sum += val
-			str = ""
-			fmt.Println("SUM", sum)
-		} else {
-			str += strconv.Itoa(nums[i])
-			val, _ := strconv.Atoi(str)
-			sum += val
-		}
-		i++
-		ed--
-		if i > ed {
-			break
-		}
+func splitNum(num int) int {
+	str := strconv.Itoa(num)
+	var arr []int
+	for _, ele := range str {
+		val, _ := strconv.Atoi(string(ele))
+		arr = append(arr, val)
 	}
-	return int64(sum)
+	sort.Ints(arr)
+	fmt.Println("nums2", arr)
+	return 0
+
 }
 
 func main() {
-	nums := []int{5, 14, 13, 8, 12}
-	fmt.Println(findTheArrayConcVal(nums))
+	num := 4325
+	fmt.Println(splitNum(num))
 }
