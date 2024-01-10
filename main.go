efa3fce4d866296c0b5eca1c9f5aa8989e468286
Robin Joseph <robinjoseph11998@gmail.com>
2024-01-11 01:17:14 +0530

    latest commit

--- main.go
+++ main.go
@@ -2,16 +2,48 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+	"sort"
+	"strconv"
 )
 
 func splitNum(num int) int {
-	val := string(num)
-	fmt.Println("val", val)
-	return 0
+	stringNum := strconv.Itoa(num)
+	var numArr []int
+	for _, ele := range stringNum {
+		oele, _ := strconv.Atoi(string(ele))
+		if oele != 0 {
+			numArr = append(numArr, oele)
+		}
+	}
+	num1 := ""
+	num2 := ""
+	allEqual := reflect.DeepEqual(numArr[1:], numArr[:len(numArr)-1])
+	if allEqual {
+		if len(numArr)%2 != 0 {
+
+		}
+	}
+	fmt.Println("len(numArr)", len(numArr))
+	if len(numArr) == 2 {
+		return numArr[0] + numArr[1]
+	}
+
+	sort.Ints(numArr)
+	for i, val := range numArr {
+		if i%2 == 0 {
+			num1 += strconv.Itoa(val)
+		} else {
+			num2 += strconv.Itoa(val)
+		}
+	}
+	val1, _ := strconv.Atoi(num1)
+	val2, _ := strconv.Atoi(num2)
+	return val1 + val2
 
 }
 
 func main() {
-	num := 4325
+	num := 999999999
 	fmt.Println(splitNum(num))
 }
