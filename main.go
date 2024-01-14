e4542e179dc3c0f79228ae2c9acf0f651cba649b
Robin Joseph <robinjoseph11998@gmail.com>
2024-01-14 23:46:49 +0530

    latest commit

--- main.go
+++ main.go
@@ -2,43 +2,20 @@ package main
 
 import "fmt"
 
-func kItemsWithMaximumSum(numOnes int, numZeros int, numNegOnes int, k int) int {
+func primeSubOperation(nums []int) bool {
 
-	var Bag []int
-	if numOnes != 0 {
-		for i := 0; i < numOnes; i++ {
-			Bag = append(Bag, 1)
+	for i := 0; i < len(nums); i++ {
+		if nums[i] <= nums[i+1] {
+			break
 		}
+		return true
 	}
-	if numZeros != 0 {
-		for i := 0; i < numZeros; i++ {
-			Bag = append(Bag, 0)
-		}
-	}
-	if numNegOnes == 0 {
+	return false
 
-	} else {
-		for i := 0; i < numNegOnes; i++ {
-			Bag = append(Bag, -1)
-		}
-	}
-	fmt.Println("Bag", Bag)
-	Sum := 0
-	for i := 0; i < k; i++ {
-		fmt.Println("k", k)
-		fmt.Println("i", i)
-		Sum += Bag[i]
-	}
-	return Sum
 }
 
 func main() {
+	nums := []int{4, 9, 6, 10}
 
-	numOnes := 1
-	numZeros := 4
-	numNegOnes := 1
-	k := 3
-
-	fmt.Println(kItemsWithMaximumSum(numOnes, numZeros, numNegOnes, k))
-
+	fmt.Println(primeSubOperation(nums))
 }
