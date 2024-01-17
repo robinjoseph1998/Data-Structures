09e873ace892864134390a35a1456214ca7e5774
Robin Joseph <robinjoseph11998@gmail.com>
2024-01-17 22:51:13 +0530

    latest commit

--- main.go
+++ main.go
@@ -2,36 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-func isStrictlyIncreasing(nums []int) bool {
-	for i := 0; i < len(nums); i++ {
-		if nums[i] >= nums[i+1] {
-			return false
-		}
-	}
-	return true
-}
-
 func primeSubOperation(nums []int) bool {
-	if isStrictlyIncreasing(nums) {
-		return true
-	}
-	var Helper []int
-	for i := 0; i < len(nums); i++ {
-		if nums[i] > 2 {
-			for j := nums[i] - 1; j > 0; j-- {
-				for p := 2; p < int(math.Sqrt(float64(j))); p++ {
-					if j%p != 0 {
-						Helper = append(Helpe, j)
-					}
-				}
-			}
-		}
-	}
-	fmt.Println("Helper", Helper)
-	return false
+
 }
 
 func main() {
