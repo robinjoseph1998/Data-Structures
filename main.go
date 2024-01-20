e4b6cb7c72c749c9b416283c887c2094ce07879d
Robin Joseph <robinjoseph11998@gmail.com>
2024-01-21 01:38:48 +0530

    latest commit

--- main.go
+++ main.go
@@ -5,12 +5,49 @@ import (
 )
 
 func findTheLongestBalancedSubstring(s string) int {
-
+	var Zeroes, Once, TotalCount int = 0, 0, 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '0' {
+			Zeroes++
+			Once = 0
+		}
+		if s[i] == '1' {
+			Once++
+		}
+		var affect int = 0
+		if Zeroes == Once {
+			affect++
+			MaxCount := Zeroes + Once
+			if MaxCount > TotalCount {
+				TotalCount = Zeroes + Once
+				Zeroes = 0
+			}
+		}
+		if i == len(s)-1 && affect == 0 {
+			var raw0, raw1 int = 0, 0
+			for k := 0; k < len(s); k++ {
+				if s[k] == '0' {
+					raw0++
+				} else {
+					raw1++
+				}
+			}
+			fmt.Println("raw0", raw0)
+			fmt.Println("r aw1", raw1)
+			if raw0 > raw1 {
+				return raw1 + raw1
+			} else {
+				fmt.Println(":::")
+				return raw0 + raw0
+			}
+		}
+	}
+	return TotalCount
 }
 
 func main() {
 
-	s := "001"
+	s := "010"
 
 	fmt.Println(findTheLongestBalancedSubstring(s))
 
