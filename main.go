2a1bd1823e03e12b5d93ea2b71b0188724d653e9
Robin Joseph <robinjoseph11998@gmail.com>
2024-01-21 23:54:04 +0530

    latest

--- main.go
+++ main.go
@@ -1,36 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
+func minLength(s string) int {
 
-func findTheLongestBalancedSubstring(s string) int {
-	var Zeroes, Once, TotalCount int = 0, 0, 0
-	Flag := false
-	for i := 0; i < len(s); i++ {
-		if s[i] == '0' {
-			Zeroes++
-			Once = 0
-		}
-		if s[i] == '1' {
-			Once++
-			Flag = true
-		}
-		if Zeroes >= Once && TotalCount < Once {
-			TotalCount = Once
-		}
-		if Flag && s[i] == '0' {
-			Zeroes = 1
-			Flag = false
-		}
-	}
-	return TotalCount * 2
 }
 
 func main() {
-
-	s := "010"
-
-	fmt.Println(findTheLongestBalancedSubstring(s))
 
 }
