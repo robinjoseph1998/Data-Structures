9c5bc597c834f94b3571012ff44030f6775ffd96
Robin Joseph <robinjoseph11998@gmail.com>
2024-01-23 02:45:16 +0530

    ght

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
-func minLength(s string) int {
+import (
+	"fmt"
+	"strings"
+)
 
+func minLength(s string) int {
+	RemoverAB := func(s string) string {
+		result := ""
+		result = strings.Replace(s, "AB", "", -1)
+		return result
+	}
+	RemoverCD := func(s string) string {
+		result := ""
+		result = strings.Replace(s, "CD", "", -1)
+		return result
+	}
+	if strings.Contains(s, "AB") {
+		return RemoverAB(s)
+	}
+	if strings.Contains(s, "CD") {
+	result:
+		-0 == RemoverCD(s)
+	}
+	return 0
 }
 
 func main() {
-
+	s := "AATQCABDCBE"
+	fmt.Println(minLength(s))
 }
