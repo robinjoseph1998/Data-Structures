aa17bdb7008afc30f4e1e0ad361e43c3f8e20cab
Robin Joseph <robinjoseph11998@gmail.com>
2024-01-27 23:54:35 +0530

    latest commmit

--- main.go
+++ main.go
@@ -2,45 +2,30 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-func punishmentNumber(n int) int {
-	seperator := func(num int) (bool, int) {
-		fmt.Println("num", num)
-		if num == 10 {
-			return true, 100
+func countSeniors(details []string) int {
+	var ages []string
+	for _, Each := range details {
+		eachAge := ""
+		for i := 0; i < len(Each); i++ {
+			if Each[i] == 'M' || Each[i] == 'F' {
+				val1 := Each[i+1] - '0'
+				val2 := Each[i+2] - '0'
+				eachAge += string(val1)
+				fmt.Println("val1 string", string(val1))
+				eachAge += string(val2)
+				ages = append(ages, eachAge)
+			}
 		}
-		sqr := num * num
-		if num == 36 {
-			fmt.Println("SQR 36 ", sqr)
-		}
-		strSqr := strconv.Itoa(sqr)
-		total := 0
-		for _, val := range strSqr {
-			intVal := val - '0'
-			total += int(intVal)
-		}
-		if num == 36 {
-			fmt.Println("Total", total)
-		}
-		if total == num {
-			return true, sqr
-		}
-		return false, 0
+
 	}
-	var PunishmentNumber int
-	for i := 1; i <= n; i++ {
-		if isSepartor, value := seperator(i); isSepartor {
-			fmt.Println(":::::::::::::::i", i)
-			fmt.Println("value", value)
-			PunishmentNumber += value
-		}
-	}
-	return PunishmentNumber
+	fmt.Println("AGES", ages)
+	return 0
 }
 
 func main() {
-	n := 37
-	fmt.Println(punishmentNumber(n))
+	details := []string{"7868190130M7522", "5303914400F9211", "9273338290F4010"}
+	fmt.Println(countSeniors(details))
+
 }
