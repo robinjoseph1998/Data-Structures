c16a9b1a87f739ac44fd52eb81ef64c547c03476
Robin Joseph <robinjoseph11998@gmail.com>
2024-01-30 02:28:23 +0530

    latest commit

--- main.go
+++ main.go
@@ -2,22 +2,16 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	"strings"
 )
 
-func sumOfPower(nums []int) int {
-	modulo := 1_000_000_007
-	sort.Ints(nums)
-	sum, result := 0, 0
-	for _, num := range nums {
-		result = (result + (num*num%modulo)*(num+sum)) % modulo
-		sum = (sum<<1 + num) % modulo
-
-	}
-	return result
+func removeTrailingZeros(num string) string {
+	return strings.TrimRight(num, "0")
 }
 
 func main() {
-	nums := []int{2, 1, 4}
-	fmt.Println(sumOfPower(nums))
+	num := "50"
+
+	fmt.Println(removeTrailingZeros(num))
+
 }
