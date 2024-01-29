4e903f0daa9a5ee43b0f38ff63d76acad0fbd70f
Robin Joseph <robinjoseph11998@gmail.com>
2024-01-29 16:26:06 +0530

    latest commit

--- main.go
+++ main.go
@@ -2,22 +2,43 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"math"
+	"sort"
 )
 
-func countSeniors(details []string) int {
-	count := 0
-	for _, EachVal := range details {
-		age, _ := strconv.Atoi(EachVal[11:13])
-		if age > 60 {
-			count++
-		}
+func matrixSum(nums [][]int) int {
+	for _, row := range nums {
+		sort.Ints(row)
+		fmt.Println("row", row)
 	}
-	return count
+	score := 0
+	for i := 0; i < len(nums[0]); i++ {
+		max := 0
+		for j := 0; j < len(nums); j++ {
+			max = int(math.Max(float64(max), float64(nums[j][i])))
+		}
+		score += max
+		fmt.Println("max", max)
+	}
+	return score
 }
-
 func main() {
-	details := []string{"5612624052M0130", "5378802576M6424", "5447619845F0171", "2941701174O9078"}
-	fmt.Println(countSeniors(details))
+
+	nums := [][]int{
+		{1, 8, 16, 15, 12, 9, 15, 11, 18, 6, 16, 4, 9, 4},
+		{3, 19, 8, 17, 19, 4, 9, 3, 2, 10, 15, 17, 3, 11},
+		{13, 10, 19, 20, 6, 17, 15, 14, 16, 8, 1, 17, 0, 2},
+		{12, 20, 0, 19, 15, 10, 7, 10, 2, 6, 18, 7, 7, 4},
+		{17, 14, 2, 2, 10, 16, 15, 3, 9, 17, 9, 3, 17, 10},
+		{17, 6, 19, 17, 18, 9, 14, 2, 19, 12, 10, 18, 7, 9},
+		{5, 6, 5, 1, 19, 8, 15, 2, 2, 4, 4, 1, 2, 17},
+		{12, 16, 8, 16, 7, 6, 18, 13, 18, 8, 14, 15, 20, 11},
+		{2, 10, 19, 3, 15, 18, 20, 10, 6, 7, 0, 8, 3, 7},
+		{11, 5, 10, 13, 1, 3, 4, 7, 1, 18, 20, 17, 19, 2},
+		{0, 3, 20, 6, 19, 18, 3, 12, 2, 11, 3, 1, 19, 0},
+		{6, 5, 3, 15, 6, 1, 0, 17, 13, 19, 3, 8, 2, 7},
+		{2, 20, 9, 11, 13, 5, 1, 16, 14, 1, 19, 3, 12, 6},
+	}
+	fmt.Println(matrixSum(nums))
 
 }
