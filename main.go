3c912d586eba3666f2c3b5ebca64ab89864d266e
Robin Joseph <robinjoseph11998@gmail.com>
2024-02-01 01:43:15 +0530

    latest commit

--- main.go
+++ main.go
@@ -2,42 +2,39 @@ package main
 
 import (
 	"fmt"
-	"sort"
-	"strconv"
 )
 
-func isFascinating(n int) bool {
-	a := n * 2
-	fmt.Println("a", a)
-	b := n * 3
-	fmt.Println("b", b)
-	strA := strconv.Itoa(a)
-	strB := strconv.Itoa(b)
-	strN := strconv.Itoa(n)
+func maximumNumberOfStringPairs(words []string) int {
+	Count := 0
+	j := 0
+	for i := 0; i < len(words)-1; i++ {
+		rev := ""
+		val := string(words[i])
 
-	concatVal := strN + strA + strB
-	result := true
-	var conArr []int
-	for _, each := range concatVal {
-		conArr = append(conArr, int(each-'0'))
-	}
-	sort.Ints(conArr)
-	count := 0
-	for i := 0; i < len(conArr); i++ {
-		if i+1 == conArr[i] {
-			fmt.Println("i+1", i+1)
-			fmt.Println("concat Value i", conArr[i])
-			count++
-		} else {
-			result = false
+		for i := len(val) - 1; i >= 0; i-- {
+			rev += string(val[i])
+		}
+
+		for j = i + 1; j < len(words); j++ {
+			if rev == string(words[j]) {
+				fmt.Println("::::::::::::::REV", rev)
+				fmt.Println("(words[j])", string(words[j]))
+				Count++
+				break
+			}
+		}
+		if j == len(words)-1 {
+			fmt.Println("j", j)
+			break
 		}
 	}
-
-	return result
+	return Count
 }
 
 func main() {
-	n := 192
-	fmt.Println(isFascinating(n))
+
+	words := []string{"cd", "ac", "dc", "ca", "zz"}
+
+	fmt.Println(maximumNumberOfStringPairs(words))
 
 }
