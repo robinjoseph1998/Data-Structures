8c2fa4d2ce57985ea698bde4dafcc1300b21eebd
Robin Joseph <robinjoseph11998@gmail.com>
2024-02-02 03:47:36 +0530

    latest commit

--- main.go
+++ main.go
@@ -2,39 +2,27 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-func maximumNumberOfStringPairs(words []string) int {
-	Count := 0
-	j := 0
-	for i := 0; i < len(words)-1; i++ {
-		rev := ""
-		val := string(words[i])
-
-		for i := len(val) - 1; i >= 0; i-- {
-			rev += string(val[i])
+func divideArray(nums []int, k int) [][]int {
+	sort.Ints(nums)
+	i := 0
+	var Out [][]int
+	for i < len(nums) {
+		if i+2 < len(nums) && nums[i+2]-nums[i] <= k {
+			Out = append(Out, []int{nums[i], nums[i+1], nums[i+2]})
+		} else {
+			return [][]int{}
 		}
-
-		for j = i + 1; j < len(words); j++ {
-			if rev == string(words[j]) {
-				fmt.Println("::::::::::::::REV", rev)
-				fmt.Println("(words[j])", string(words[j]))
-				Count++
-				break
-			}
-		}
-		if j == len(words)-1 {
-			fmt.Println("j", j)
-			break
-		}
+		i += 3
 	}
-	return Count
+	return Out
 }
 
 func main() {
-
-	words := []string{"cd", "ac", "dc", "ca", "zz"}
-
-	fmt.Println(maximumNumberOfStringPairs(words))
+	nums := []int{1, 3, 4, 8, 7, 9, 3, 5, 1}
+	k := 2
+	fmt.Println(divideArray(nums, k))
 
 }
