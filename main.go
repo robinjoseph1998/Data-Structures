c607f07ffcc0b18a1f55c85bde709a0a9b4c90a0
Robin Joseph <robinjoseph11998@gmail.com>
2024-02-03 00:02:23 +0530

    latest

--- main.go
+++ main.go
@@ -2,29 +2,46 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strconv"
 )
 
-func finalString(s string) string {
-
-	str := ""
-	rev := ""
-
-	for i := 0; i < len(s); i++ {
-		if s[i] == 'i' {
-			for j := len(str) - 1; j >= 0; j-- {
-				rev += string(str[j])
+func maxSum(nums []int) int {
+	total := -1
+	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
+	for i := 0; i < len(nums); i++ {
+		if i+1 < len(nums) {
+			for j := i + 1; j < len(nums); j++ {
+				if nums[i] > 9 && nums[j] > 9 {
+					strI := strconv.Itoa(nums[i])
+					strJ := strconv.Itoa(nums[j])
+					Ia, _ := strconv.Atoi(string(strI[0]))
+					Ib, _ := strconv.Atoi(string(strI[1]))
+					Ja, _ := strconv.Atoi(string(strJ[0]))
+					Jb, _ := strconv.Atoi(string(strJ[1]))
+					maxI := max(Ia, Ib)
+					maxJ := max(Ja, Jb)
+					if maxI == maxJ {
+						sum := nums[i] + nums[j]
+						if sum > total {
+							total = sum
+						}
+					}
+				}
 			}
-			fmt.Println("rev", rev)
-			str = rev
-			rev = ""
-		} else {
-			str += string(s[i])
 		}
 	}
-	return str
+	return total
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func main() {
-	s := "poiinter"
-	fmt.Println(finalString(s))
+	nums := []int{51, 71, 17, 24, 42}
+	fmt.Println(maxSum(nums))
+
 }
