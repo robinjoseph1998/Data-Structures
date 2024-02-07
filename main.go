d3bbfd1b857488e5d2eaf4a6bfe42f334d23a72c
Robin Joseph <robinjoseph11998@gmail.com>
2024-02-08 01:01:13 +0530

    latest

--- main.go
+++ main.go
@@ -1,41 +1,19 @@
 package main
 
-import "fmt"
-
-func romanToInt(s string) int {
-	Roman := map[string]int{
-		"I": 1,
-		"V": 5,
-		"X": 10,
-		"L": 50,
-		"C": 100,
-		"D": 500,
-		"M": 1000,
-	}
-	ans := 0
-	for i := 0; i < len(s)-1; i++ {
-		if i+1 < len(s) {
-			A := Roman[string(s[i])]
-			B := Roman[string(s[i+1])]
-			G := Max(A, B)
-			if G == A {
-				ans += A
-			} else {
-				ans -= A
-			}
-		}
-	}
-	ans += Roman[string(s[len(s)-1])]
-	return ans
+type Stack struct {
+	Item []interface{}
 }
-func Max(a, b int) int {
-	if a >= b {
-		return a
+
+func (s *Stack) push(a interface{}) {
+	s.Item = append(s.Item, a)
+}
+
+func (s *Stack) pop() interface{} {
+	if len(s.Item) == 0 {
+		return nil
 	}
-	return b
 }
 
 func main() {
-	s := "MCMXCIV"
-	fmt.Println(romanToInt(s))
+
 }
