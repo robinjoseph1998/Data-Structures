4d00d5d2691f9e9209f21e874c879e3433343f82
Robin Joseph <robinjoseph11998@gmail.com>
2024-02-09 00:35:02 +0530

    latest commit

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Stack struct {
 	Item []interface{}
 }
@@ -12,8 +14,33 @@ func (s *Stack) pop() interface{} {
 	if len(s.Item) == 0 {
 		return nil
 	}
+	item := s.Item[len(s.Item)-1]
+	s.Item = s.Item[:len(s.Item)-1]
+	return item
+}
+
+func (s *Stack) peek() interface{} {
+	if len(s.Item) == 0 {
+		return nil
+	}
+	return s.Item[len(s.Item)-1]
+}
+
+func (s *Stack) isEmpty() bool {
+	return len(s.Item) == 0
+}
+
+func (s *Stack) Size() int {
+	return len(s.Item)
 }
 
 func main() {
+	stack := &Stack{}
+	stack.push(900)
+	stack.push(600)
+	stack.push(700)
+
+	fmt.Println("POP", stack.pop())
+	fmt.Println("peek After POP", stack.peek())
 
 }
