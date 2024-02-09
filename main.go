4a723f6a1102d04c883137def8bc07d68655dc03
Robin Joseph <robinjoseph11998@gmail.com>
2024-02-10 01:55:20 +0530

    latest commit

--- main.go
+++ main.go
@@ -1,31 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func longestValidParentheses(s string) int {
-	stack := []int{-1}
-	MaxLength := 0
-	for cuIdx, pare := range s {
-		if pare == '(' {
-			stack = append(stack, cuIdx)
-		} else {
-			stack = stack[:len(stack)-1]
-			if len(stack) == 0 {
-				stack = append(stack, cuIdx)
-			} else {
-				MaxLength = Max(MaxLength, cuIdx-stack[len(stack)-1])
-			}
+func lastVisitedIntegers(words []string) []int {
+	count := 0
+	for _, each := range words {
+		if each == "prev" {
+			count++
 		}
 	}
-	return MaxLength
-}
-func Max(a, b int) int {
-	if a > b {
-		return a
+	var result []int
+	for i := len(words) - 1; i > 0; i-- {
+		if words[i] != "prev" {
+			val, _ := strconv.Atoi(words[i])
+			result = append(result, val)
+			count--
+		}
+		// if i < count {
+		// 	result = append(result, -1)
+		// }
 	}
-	return b
+	return result
 }
+
 func main() {
-	s := "()(())"
-	fmt.Println(longestValidParentheses(s))
+	words := []string{"1", "2", "prev", "prev", "prev"}
+	fmt.Println(lastVisitedIntegers(words))
 }
