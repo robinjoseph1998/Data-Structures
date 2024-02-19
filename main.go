beb4944c14ab32db449d6ef2ff14562757bbd407
Robin Joseph <robinjoseph11998@gmail.com>
2024-02-20 02:24:00 +0530

    latest

--- main.go
+++ main.go
@@ -2,26 +2,22 @@ package main
 
 import "fmt"
 
-func removePalindromeSub(s string) int {
-	if len(s) == 0 {
-		return 0
-	} else if isPalindrome(s) {
-		return 1
-	}
-	return 2
-}
-
-func isPalindrome(s string) bool {
-	for i := 0; i < len(s); i++ {
-		if s[i] != s[len(s)-1-i] {
-			return false
+func findPeaks(mountain []int) []int {
+	var peeks []int
+	for i := 1; i < len(mountain); i++ {
+		if i+1 < len(mountain) {
+			if mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1] {
+				peeks = append(peeks, i)
+			}
 		}
 	}
-	return true
+	return peeks
 }
 
 func main() {
 
-	s := "ababa"
-	fmt.Println(removePalindromeSub(s))
+	mountain := []int{2, 4, 4}
+
+	fmt.Println(findPeaks(mountain))
+
 }
