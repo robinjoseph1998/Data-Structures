7a79e98452646782735813e71ee3c5836c237ff9
Robin Joseph <robinjoseph11998@gmail.com>
2024-02-22 02:34:52 +0530

    latest

--- main.go
+++ main.go
@@ -2,21 +2,24 @@ package main
 
 import "fmt"
 
-func makeEqual(words []string) bool {
-	freq := make(map[string]int)
-
-	for _, each := range words {
-		for i := 0; i < len(each); i++ {
-			freq[string(each[i])]++
+func findIntersectionValues(nums1 []int, nums2 []int) []int {
+	var res []int
+	count := 0
+	for i := 0; i < len(nums1); i++ {
+		for j := 0; j < len(nums2); j++ {
+			if nums1[i] == nums2[j] {
+				count++
+				break
+			}
 		}
 	}
-	fmt.Println("freq", freq)
-	return false
+	fmt.Println("res", res)
+	return []int{}
 }
 
 func main() {
+	nums1 := []int{4, 3, 2, 3, 1}
+	nums2 := []int{2, 2, 5, 2, 3, 6}
 
-	words := []string{"abc", "aabc", "bc"}
-	fmt.Println(makeEqual(words))
-
+	fmt.Println(findIntersectionValues(nums1, nums2))
 }
