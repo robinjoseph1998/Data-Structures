2e441f170bd153d3d4eb16d0bbc514381f850c35
Robin Joseph <robinjoseph11998@gmail.com>
2024-02-27 01:40:51 +0530

    latest

--- main.go
+++ main.go
@@ -1,33 +1,16 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func findMissingAndRepeatedValues(grid [][]int) []int {
-	Freq := make(map[int]int)
-	for _, each := range grid {
-		for _, val := range each {
-			Freq[val]++
-		}
-	}
-	n := len(grid) * len(grid)
-	var repeat, missing int
+func incremovableSubarrayCount(nums []int) int {
+	for i := 0; i < len(nums); i++ {
 
-	for i := 1; i <= n; i++ {
-		if Freq[i] == 2 {
-			repeat = i
-		}
-		if Freq[i] == 0 {
-			missing = i
-		}
 	}
-	return []int{repeat, missing}
 }
 
 func main() {
 
-	grid := [][]int{{9, 1, 7}, {8, 9, 2}, {3, 4, 6}}
-	fmt.Println(findMissingAndRepeatedValues(grid))
+	nums := []int{1, 2, 3, 4}
+	fmt.Println(incremovableSubarrayCount(nums))
 
 }
