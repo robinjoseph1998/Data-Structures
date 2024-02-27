13671a4b18850e81aaec1304ca5ad8e8568f4f3f
Robin Joseph <robinjoseph11998@gmail.com>
2024-02-27 16:34:22 +0530

    latest

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
-func incremovableSubarrayCount(nums []int) int {
-	for i := 0; i < len(nums); i++ {
-
+func numberGame(nums []int) []int {
+	var arr []int
+	sort.Sort(sort.Reverse(sort.Reverse(sort.IntSlice(nums))))
+	fmt.Println("nums", nums)
+	for i := 0; i < len(nums); i += 2 {
+		if i+1 < len(nums) && nums[i] < nums[i+1] {
+			if nums[i] < nums[i+1] {
+				arr = append(arr, nums[i+1])
+				arr = append(arr, nums[i])
+			}
+		}
+		if nums[i] == nums[i+1] {
+			arr = append(arr, nums[i])
+			arr = append(arr, nums[i+1])
+		}
 	}
+	return arr
 }
 
 func main() {
 
-	nums := []int{1, 2, 3, 4}
-	fmt.Println(incremovableSubarrayCount(nums))
+	nums := []int{2, 7, 9, 6, 4, 6}
+	fmt.Println(numberGame(nums))
 
 }
