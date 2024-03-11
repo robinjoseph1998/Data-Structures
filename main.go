63b515e97d32723cf026db9610e07709f1c744bd
Robin Joseph <robinjoseph11998@gmail.com>
2024-03-12 00:12:35 +0530

    latest

--- main.go
+++ main.go
@@ -1,45 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
-type Node struct {
-	data int
-	next *Node
-}
-
-type linkedlist struct {
-	head *Node
-}
-
-func (ll *linkedlist) Append(data int) {
-	newNode := &Node{data: data, next: nil}
-	if ll.head == nil {
-		ll.head = newNode
-		return
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	var arr1 []int
+	for i := 0; i < m; i++ {
+		arr1 = append(arr1, nums1[i])
 	}
-	current := ll.head
-	for current.next != nil {
-		current = current.next
+	for i := 0; i < n; i++ {
+		arr1 = append(arr1, nums2[i])
 	}
-	current.next = newNode
-
-}
-
-func (ll *linkedlist) Display() {
-	current := ll.head
-	for current != nil {
-		fmt.Println("data", current.data)
-		current = current.next
-	}
-	fmt.Println("nil")
+	copy(nums1, arr1)
+	sort.Ints(nums1)
+	fmt.Println("nums1[i]", nums1)
 }
 
 func main() {
-	ll := linkedlist{}
-	ll.Append(10)
-	ll.Append(20)
-	ll.Append(30)
-	ll.Append(40)
 
-	ll.Display()
+	nums1 := []int{-50, -48, -47, -47, -46, -44, -43, -43, -41, -39, -38, -37, -37, -37, -33, -33, -32, -31, -29, -29, -27, -26, -26, -26, -25, -25, -24, -24, -23, -22, -22, -22, -18, -17, -17, -14, -14, -11, -11, -11, -6, -5, -5, -5, -5, -4, -1, 0, 2, 2, 2, 2, 5, 6, 7, 7, 9, 10, 13, 13, 14, 14, 18, 21, 21, 21, 22, 24, 24, 24, 25, 26, 26, 29, 29, 29, 30, 30, 31, 31, 32, 33, 34, 34, 34, 35, 38, 40, 41, 42, 43, 44, 44, 46, 46, 47, 47, 48, 49, 49}
+	m := 100
+	nums2 := []int{}
+
+	n := 0
+	merge(nums1, m, nums2, n)
+
 }
