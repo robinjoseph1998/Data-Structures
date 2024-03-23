f0fa643d79a1e93ceb10ba48404643301a9c64cb
Robin Joseph <robinjoseph11998@gmail.com>
2024-03-24 02:17:47 +0530

    latest

--- main.go
+++ main.go
@@ -1,24 +1,46 @@
 package main
 
-import "fmt"
-
-func singleNumber(nums []int) int {
-	myMap := make(map[int]int)
-	for _, val := range nums {
-		myMap[val]++
-	}
-	res := 0
-	for key, val := range myMap {
-		if val == 1 {
-			res = key
-			break
-		}
-	}
-	return res
-}
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	nums := []int{4, 1, 2, 1, 2}
-	fmt.Println(singleNumber(nums))
 
+	var Score = []int{0}
+	wg := &sync.WaitGroup{}
+	mut := &sync.RWMutex{}
+	wg.Add(4)
+	go func(wg *sync.WaitGroup, m *sync.RWMutex) {
+		fmt.Println("First routine")
+		m.Lock()
+		Score = append(Score, 1)
+		m.Unlock()
+		wg.Done()
+	}(wg, mut)
+	go func(wg *sync.WaitGroup, m *sync.RWMutex) {
+		fmt.Println("Second routine")
+		m.Lock()
+		Score = append(Score, 2)
+		m.Unlock()
+		wg.Done()
+	}(wg, mut)
+
+	go func(wg *sync.WaitGroup, m *sync.RWMutex) {
+		fmt.Println("Third routine")
+		m.Lock()
+		Score = append(Score, 3)
+		m.Unlock()
+		wg.Done()
+	}(wg, mut)
+
+	go func(wg *sync.WaitGroup, m *sync.RWMutex) {
+		m.RLock()
+		fmt.Println("Score:", Score)
+		m.RUnlock()
+		wg.Done()
+	}(wg, mut)
+
+	wg.Wait()
+	fmt.Println("Scores", Score)
 }
