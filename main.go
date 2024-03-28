a007f69287dcbbe92442598958665eeedcd0404c
Robin Joseph <robinjoseph11998@gmail.com>
2024-03-28 16:40:58 +0530

    latest

--- main.go
+++ main.go
@@ -2,37 +2,32 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
-func isSubstringPresent(s string) bool {
-	if isPalindrome(s) {
-		return true
+func addToArrayForm(num []int, k int) []int {
+	strNum := ""
+	for i := 0; i < len(num); i++ {
+		strNum += strconv.Itoa(num[i])
 	}
-	for i := 0; i < len(s); i++ {
-		if i+1 < len(s) {
-			if s[i] != s[i+1] {
-				continue
-			} else {
-				return true
-			}
-		}
+	fmt.Println("strNum", strNum)
+	intNum, _ := strconv.ParseUint(strNum, 10, 64)
+	fmt.Println("intNum", intNum)
+	sum := intNum + uint64(k)
+	fmt.Println("sum", sum)
+	strSum := strconv.FormatUint(sum, 10)
+	var arr []int
+	for _, val := range strSum {
+		intv, _ := strconv.Atoi(string(val))
+		arr = append(arr, intv)
 	}
-	return false
-}
-
-func isPalindrome(text string) bool {
-	size := len(text)
-	for i := 0; i < size/2; i++ {
-		if text[i] != text[size-1-i] {
-
-			return false
-		}
-	}
-	return true
+	return arr
 }
 
 func main() {
-	s := "abcba"
-	fmt.Println(isSubstringPresent(s))
+
+	num := []int{3, 8, 0, 3, 0, 2, 7, 0, 7, 6, 4, 9, 9, 1, 7, 6, 6, 1, 6, 4}
+	k := 670
+	fmt.Println(addToArrayForm(num, k))
 
 }
