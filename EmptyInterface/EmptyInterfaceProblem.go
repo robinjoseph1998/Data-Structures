c42ae7152ab4bbfd546ecd78471f6c2a711ba94a
Robin Joseph <robinjoseph11998@gmail.com>
2024-06-02 02:30:47 +0530

    leetcode-830

--- EmptyInterface/EmptyInterfaceProblem.go
+++ EmptyInterface/EmptyInterfaceProblem.go
@@ -1,37 +1,37 @@
 package main
 
-// import "fmt"
+import "fmt"
 
-// func detailer(val ...interface{}) {
+func detailer(val ...interface{}) {
 
-// 	for _, values := range val {
+	for _, values := range val {
 
-// 		switch val := values.(type) {
+		switch val := values.(type) {
 
-// 		case string:
-// 			fmt.Println("Name", val)
-// 		case int:
-// 			fmt.Println("Age", val)
-// 		case []int:
-// 			var total int
-// 			for _, marks := range val {
-// 				total += marks
-// 			}
-// 			avg := total/len(val) - 1
-// 			fmt.Println("Avg Marks", avg)
-// 		}
-// 	}
-// }
+		case string:
+			fmt.Println("Name", val)
+		case int:
+			fmt.Println("Age", val)
+		case []int:
+			var total int
+			for _, marks := range val {
+				total += marks
+			}
+			avg := total/len(val) - 1
+			fmt.Println("Avg Marks", avg)
+		}
+	}
+}
 
-// func main() {
+func main() {
 
-// 	datas := map[string]interface{}{
+	datas := map[string]interface{}{
 
-// 		"Name":  "Robin Joseph",
-// 		"Age":   25,
-// 		"Marks": []int{100, 200, 300, 100, 400},
-// 	}
+		"Name":  "Robin Joseph",
+		"Age":   25,
+		"Marks": []int{100, 200, 300, 100, 400},
+	}
 
-// 	detailer(datas["Name"], datas["Age"], datas["Marks"])
+	detailer(datas["Name"], datas["Age"], datas["Marks"])
 
-// }
+}
