2311f4278c151de7a10d4012567e6f927c572fc5
Robin Joseph <robinjoseph11998@gmail.com>
2024-07-05 16:04:29 +0530

    leetcode-260

--- main.go
+++ main.go
@@ -2,44 +2,27 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
-func findClosestElements(arr []int, k int, x int) []int {
-	type Pairs struct {
-		value      int
-		difference int
+func singleNumber(nums []int) []int {
+	if len(nums) == 2 {
+		return nums
 	}
-	var PairSlice []Pairs
-	for _, val := range arr {
-		diff := abs(val - x)
-		PairSlice = append(PairSlice, Pairs{value: val, difference: diff})
+	occurence := make(map[int]int)
+	for _, num := range nums {
+		occurence[num]++
 	}
-
-	sort.Slice(PairSlice, func(i, j int) bool {
-		if PairSlice[i].difference == PairSlice[j].difference {
-			return PairSlice[i].value < PairSlice[j].value
+	res := make([]int, 2)
+	i := 0
+	for k, v := range occurence {
+		if v == 1 {
+			res[i] = k
+			i++
 		}
-		return PairSlice[i].difference < PairSlice[j].difference
-	})
-
-	result := []int{}
-	for i := 0; i < k; i++ {
-		result = append(result, PairSlice[i].value)
 	}
-	sort.Ints(result)
-	return result
-}
-
-func abs(a int) int {
-	if a < 0 {
-		return -a
-	}
-	return a
+	return res
 }
 func main() {
-	arr := []int{0, 0, 1, 2, 3, 3, 4, 7, 7, 8}
-	k := 3
-	x := 5
-	fmt.Println(findClosestElements(arr, k, x))
+	nums := []int{1, 2, 1, 3, 2, 5}
+	fmt.Println(singleNumber(nums))
 }
