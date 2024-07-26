3e9c437e090eda84e9a4cffef433516c03ba3ca3
Robin Joseph <robinjoseph11998@gmail.com>
2024-07-26 15:18:09 +0530

    leetcode-561

--- Leetcode_Problems/leetcode-561.go
+++ Leetcode_Problems/leetcode-561.go
@@ -7,10 +7,8 @@ import (
 
 func arrayPairSum(nums []int) int {
 	sort.Ints(nums)
-	fmt.Println("nums", nums)
 	res := 0
 	for i := 0; i < len(nums); i += 2 {
-		fmt.Println("nums[i]", nums[i])
 		res += nums[i]
 	}
 	return res
