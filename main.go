45748a93843f52ab5d9de17b2f306deb8f6be6f5
robin <robinjoseph11998@gmail.com>
2024-11-01 15:57:27 +0530

    leetcode-2239

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func findClosestNumber(nums []int) int {
+	close := math.MaxInt64
+
+	for _, num := range nums {
+		if math.Abs(float64(num)) < math.Abs(float64(close)) ||
+			math.Abs(float64(num)) == math.Abs(float64(close)) && num > close {
+			close = num
+
+		}
+	}
+	return close
+}
+
+func main() {
+	nums := []int{-4, -2, 1, 4, 8}
+	fmt.Println(findClosestNumber(nums))
+}
